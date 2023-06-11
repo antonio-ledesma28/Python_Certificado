fd3301145b22a27171b8de075712f8454ffcc82d
Antonio Yamir Ledesma Briones <54130633+antonio-ledesma28@users.noreply.github.com>
2023-06-10 18:52:58 -0600

    Creación Proyecto Final

---
+++ Proyecto Final/Ahorcado/Main.py
@@ -0,0 +1,58 @@
+#Se realiza conexión con los otros dos archivos en el main
+from Palabras import dict
+from Ahorcado_Diagramas import diagramas
+#Se importa biblioteca Random
+import random
+
+#Guarda número al azar del 1 al 4
+numeroAzar = random.randint(1,4)
+#Guarda en una variable el color dependiendo el número al azar
+palabraOculta = dict[numeroAzar].lower()
+#print(palabraOculta)
+
+print("Juego del ahorcado")
+print("Adivina la palabra oculta")
+print("Tienes 7 intentos")
+
+#Función para poder separar letra por letra en un arreglo
+letras = set(palabraOculta)
+print(len(letras))
+
+vidas = 7
+dibujo = 0
+
+while vidas != 0 and len(letras) != 0:    
+    #print(letras)
+    letraIngresada = str(input("Ingresa letra:")).lower()
+
+    if letraIngresada in letras:
+        if(len(letras) != 0):
+            print("Acertaste")
+            letras.remove(letraIngresada)
+            print(letras)
+        else:
+            print("FELICIDADES. Adivinaste la palabra: ", palabraOculta)
+
+    else:
+        print(diagramas[dibujo])
+        dibujo = dibujo + 1
+        vidas = vidas - 1
+        if(vidas > 1):
+            print("Fallastes, te quedan sólo ", vidas, "vidas")
+        if (vidas == 1):
+            print("Fallastes, te queda sólo ", vidas, "vida")
+
+if vidas == 0:
+    print("Fallaste. GAME OVER")
+
+if (len(letras) == 0):
+    print("FELICIDADES, GANASTE")
+
+
+"""
+palabra = str(input("Ingresa palabra: "))
+if palabra == palabraOculta:
+    print("Acertaste")
+else:
+    print("Fallaste")
+"""
---
+++ Proyecto Final/Ahorcado/Palabras.py
@@ -0,0 +1,5 @@
+"""
+Se crea un diccionario el cual serán las palabras que contendrá 
+el juego y deberán adivinar los jugadores.
+"""
+dict = {1:"Azul", 2:"Blanco",3:"Negro",4:"Verde"}
